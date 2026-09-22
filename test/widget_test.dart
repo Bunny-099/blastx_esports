9d80b74c0b0c6ccf5499cf1c928d558c69ad3803
Bunny <kushalkant99@gmail.com>
2026-09-22 16:53:13 +0530

    test: update branding references to BlastIX

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:blastx_esports/main.dart';
+import 'package:blastix_esports/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
